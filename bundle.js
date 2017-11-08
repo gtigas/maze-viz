8633af30ef1bd3337b3b7e6bb75dc27c116def86
Glenn Tigas <glenn.tigas@gmail.com>
2017-11-08 16:32:57 -0500

    Cells are able to find their neighbors

--- bundle.js
+++ bundle.js
@@ -75,9 +75,9 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 document.addEventListener("DOMContentLoaded", () => {
   const canvasEl = document.getElementById("canvas");
   const ctx = canvasEl.getContext("2d");
-  ctx.fillStyle = 'white'
+  ctx.fillStyle = 'black'
   const asdf = new __WEBPACK_IMPORTED_MODULE_0__maze__["a" /* default */](ctx);
-  console.log(asdf)
+  window.asdf = asdf
 });
 
 
@@ -93,6 +93,7 @@ class Maze {
   constructor(ctx){
     this.grid = this._populateGrid(ctx);
     this.ctx = ctx
+    this.getCell = this.getCell.bind(this)
   }
 
   _populateGrid(ctx){
@@ -102,10 +103,15 @@ class Maze {
       for (var j = 0; j < 31; j++) {
         let renderPositionX = ((j * 25) + 5)
         let renderPositionY = ((i * 25) + 5)
-        grid[i][j] = new __WEBPACK_IMPORTED_MODULE_0__cell__["a" /* default */](renderPositionX, renderPositionY, ctx);
+        grid[i][j] = new __WEBPACK_IMPORTED_MODULE_0__cell__["a" /* default */]([i,j], renderPositionX, renderPositionY, ctx, this);
       }
     }
     return grid
+  }
+
+  getCell(pos){
+    let [x, y] = pos
+    return this.grid[x][y]
   }
 }
 
@@ -118,12 +124,35 @@ class Maze {
 
 "use strict";
 class Cell {
-  constructor(x, y, ctx){
+  constructor(pos, x, y, ctx, maze){
     this.visited = false;
-    this.xPos = x;
-    this.yPos = y;
+    this.pos = pos;
+    this.xPosRender = x;
+    this.yPosRender = y;
     this.ctx = ctx;
+    this.maze = maze;
     this.ctx.fillRect(x, y, 20, 20);
+  }
+
+  neighbors(){
+    const DIRS = {
+      'N': [1,0],
+      'E': [0,1],
+      'S': [-1,0],
+      'W': [0,-1],
+    }
+    const neighbors = [];
+    Object.keys(DIRS).forEach( dir =>{
+      let neighborPos = [this.pos[0] +  DIRS[dir][0], this.pos[1] + DIRS[dir][1]]
+      if ((neighborPos[0] < 0 || neighborPos[0] > 18) ||
+          (neighborPos[1] < 0 || neighborPos[1] > 30)  ) {
+        return
+      }
+      let neighbor = this.maze.getCell(neighborPos);
+      neighbors.push(neighbor)
+    });
+
+    return neighbors
   }
 }
 

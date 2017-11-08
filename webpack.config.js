c351f82b5753d60dcb75093bcd089cae394c8a5a
Glenn Tigas <glenn.tigas@gmail.com>
2017-11-08 13:03:03 -0500

    Setup index.html with entry file and webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./lib/entry.js",
+  output: {
+    path: path.join(__dirname),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js"]
+  }
+};
